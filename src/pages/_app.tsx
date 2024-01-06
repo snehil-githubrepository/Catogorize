23b6906470d66b62e1bf5ef336969a16a08fc5be
Snehil's Github <69778988+snehil-githubrepository@users.noreply.github.com>
2024-01-07 05:27:05 +0530

    Wrapped app around the Recoil State management

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
+import { SessionProvider } from "next-auth/react";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SessionProvider session={pageProps.session}>
+      <RecoilRoot>
+        <Toaster />
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </SessionProvider>
+  );
 }
--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -23,7 +23,7 @@ export default async function handler(
     });
     return res.status(200).json(user);
   } catch (e) {
-    console.log(e);
-    return res.status(405).end();
+    console.error("Error registering user:", e);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 }
