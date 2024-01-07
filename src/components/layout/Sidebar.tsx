7f1b065995cd9391aca5685dd869b8d7f4b7c456
Snehil's Github <69778988+snehil-githubrepository@users.noreply.github.com>
2024-01-08 03:01:09 +0530

    Feat: Sidebar added to homePage

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,39 +1,65 @@
 import React from "react";
 import { BsBellFill, BsHouseFill } from "react-icons/bs";
+import { useRouter } from "next/router";
 import { FaUser } from "react-icons/fa";
 import SidebarLogo from "@/components/layout/SidebarLogo";
+import SidebarItem from "@/components/layout/SidebarItem";
 import { BiLogOut } from "react-icons/bi";
+import SidebarPostButton from "@/components/layout/SidebarPostButton";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import { signOut } from "next-auth/react";
 
 const Sidebar = () => {
   const { data } = useCurrentUser();
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await signOut();
+    router.push("/login"); // Navigate to the login page after logout
+  };
   const items = [
     {
       label: "Home",
-      href: "/",
+      href: "/home",
       icon: BsHouseFill,
       isProtected: false,
     },
     {
       label: "Notifications",
-      href: "/notifications",
+      href: "/home/notifications",
       isProtected: true,
       alert: data?.hasNotification,
       icon: BsBellFill,
     },
     {
       label: "Profile",
-      href: `/users/${data?.id}`,
+      href: `/home/users/${data?.id}`,
       isProtected: true,
       icon: FaUser,
     },
   ];
   return (
     <div className="col-span-1 h-full pr-4 md:pr-6">
-      <div className="flex flex-col items-end">
+      <div className="flex flex-col items-center">
         <div className="space-y-2 lg:w-[230px]">
-          <SidebarLogo></SidebarLogo>
+          <SidebarLogo />
+          {items.map((item) => (
+            <SidebarItem
+              key={item.href}
+              href={item.href}
+              label={item.label}
+              icon={item.icon}
+              isProtected={item.isProtected}
+              alert={item.alert}
+            ></SidebarItem>
+          ))}
+
+          <SidebarItem
+            onClick={handleLogout}
+            icon={BiLogOut}
+            label="logout"
+          ></SidebarItem>
+          <SidebarPostButton />
         </div>
       </div>
     </div>
---
+++ src/components/layout/SidebarPostButton.tsx
@@ -0,0 +1,33 @@
+import React, { useCallback } from "react";
+import { useRouter } from "next/router";
+import { FaCat } from "react-icons/fa";
+import { useSetRecoilState } from "recoil";
+import { loginAtom } from "@/store/modelAtom";
+import useCurrentUser from "@/hooks/useCurrentUser";
+
+const SidebarPostButton = () => {
+  const setLoginIsOpen = useSetRecoilState(loginAtom);
+  const router = useRouter();
+  const { data: currentUser } = useCurrentUser();
+  const onClick = useCallback(() => {
+    if (!currentUser) {
+      setLoginIsOpen({ isOpen: true });
+    }
+    router.push("/");
+  }, [currentUser, router, setLoginIsOpen]);
+
+  return (
+    <div onClick={onClick}>
+      <div className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center bg-sky-500 hover:bg-opacity-50 transition cursor-pointer">
+        <FaCat size={24} color="white" />
+      </div>
+      <div className="mt-6 hidden lg:block rounded-full px-4 py-2 flex bg-sky-500 hover:bg-opacity-90 transition cursor-pointer">
+        <p className="hidden lg:block text-center font-semibold text-white text-[20px]">
+          Post
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SidebarPostButton;
