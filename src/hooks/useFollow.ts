f58c8b724cc7b75f7351d8ed62b51a4cd2885756
Snehil's Github <69778988+snehil-githubrepository@users.noreply.github.com>
2024-01-08 07:05:20 +0530

    feat: Implemented user following functionality with a custom hook

---
+++ src/hooks/useFollow.ts
@@ -0,0 +1,54 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useUser from "@/hooks/useUser";
+import { useSetRecoilState } from "recoil";
+import { loginAtom } from "@/store/modelAtom";
+import { useCallback, useMemo } from "react";
+import toast from "react-hot-toast";
+import axios from "axios";
+
+const useFollow = (userId: string) => {
+  const { data: currentUser, mutate: mutateCurrentUser } = useCurrentUser();
+  const { mutate: mutateFetchedUser } = useUser(userId);
+
+  const setIsLoginModalOpen = useSetRecoilState(loginAtom);
+
+  const isFollowing = useMemo(() => {
+    const followingIds = currentUser?.followingIds || [];
+    return followingIds.includes(userId);
+  }, [currentUser?.followingIds, userId]);
+
+  const toggleFollow = useCallback(async () => {
+    if (!currentUser) {
+      setIsLoginModalOpen({ isOpen: true });
+    }
+    try {
+      let request;
+      if (!isFollowing) {
+        request = () => axios.post("/api/follow", { userId });
+      } else {
+        request = () => axios.delete("/api/follow", { data: { userId } });
+      }
+      await request();
+      mutateCurrentUser();
+      mutateFetchedUser();
+      toast.success("Success");
+    } catch (e) {
+      console.log(e);
+      toast.error("Something went wrong");
+    }
+  }, [
+    currentUser,
+    isFollowing,
+    mutateCurrentUser,
+    mutateFetchedUser,
+    setIsLoginModalOpen,
+    userId,
+  ]);
+
+  return {
+    isFollowing,
+    toggleFollow,
+  };
+};
+
+export default useFollow;
