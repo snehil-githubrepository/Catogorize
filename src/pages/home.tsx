87dff1a72f7bfb454570c760c93f23da77369b94
Snehil's Github <69778988+snehil-githubrepository@users.noreply.github.com>
2024-01-09 02:22:35 +0530

    Handling /home only when logged in with Session

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,6 +3,15 @@ import TextArea from "@/components/TextArea";
 import Layout from "@/components/Layout";
 import PostFeed from "@/components/posts/PostFeed";
 
+import { getSession } from "next-auth/react";
+import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
+import { Session } from "next-auth";
+
+interface Props {
+  session: Session | null;
+}
+
 export default function Home() {
   return (
     <Layout>
@@ -12,3 +21,24 @@ export default function Home() {
     </Layout>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context
+) => {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+};
