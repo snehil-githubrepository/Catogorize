bb1bd17c0d40393d86630008fead24b2f20465e3
Snehil's Github <69778988+snehil-githubrepository@users.noreply.github.com>
2024-01-09 05:02:13 +0530

    Bug: Fixed bug of number count by including comments in api route#2

--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -31,6 +31,7 @@ export default async function handler(
           },
           include: {
             user: true,
+            comments: true,
           },
           orderBy: {
             createdAt: "desc",
@@ -40,6 +41,7 @@ export default async function handler(
         posts = await prisma.post.findMany({
           include: {
             user: true,
+            comments: true,
           },
           orderBy: {
             createdAt: "desc",
