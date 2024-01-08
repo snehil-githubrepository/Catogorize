492367e108760c02c09829d906ed92cceb7550e4
Snehil's Github <69778988+snehil-githubrepository@users.noreply.github.com>
2024-01-08 13:19:35 +0530

    Bug: Fix of userId field with Avatar for dynamic value

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -45,7 +45,7 @@ const TextArea: React.FC<TxtAreaProps> = ({
     <div className="border-b-[1px] border-neutral-800 bg-white px-5 py-2 mb-5 ml-2 mr-2 rounded-lg">
       <div className="flex flex-row gap-4">
         <div className="mt-4">
-          <Avatar userId="1" />
+          <Avatar userId={currentUser?.id} />
         </div>
         <div className="w-full">
           <textarea
